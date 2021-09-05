16a51106cefbd498a7caecaa2f5bec777adee670
baozouai <baozouai@gmail.com>
2021-09-05 16:37:34 +0800

    feat: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module',
+    ecmaFeatures: { // 添加ES特性支持，使之能够识别ES6语法
+      jsx: true
+    }
+  },
   globals: {
    __LOG_NAMES__: 'readonly'
   },
